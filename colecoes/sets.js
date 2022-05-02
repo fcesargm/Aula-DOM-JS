da55e8d2d66f7d153b32c56f4c467f717fc9faf0
fcesargm <felipec.dev@outlook.com>
2022-05-02 16:07:46 -0300

    Selecionado apenas diretório do exercídio em questão

--- colecoes/sets.js
+++
@@ -1,8 +0,0 @@
-function uniqueElements(array) {
-	let unique = new Set(array);
-	return [...unique];
-}
-
-const arr = [30, 30, 40, 5, 223, 2049, 3034, 5];
-
-console.log(uniqueElements(arr));
--- map-filter-reduce/filter/filtraPares.js
+++
@@ -1,9 +0,0 @@
-function filtraPares(arr) {
-	if (!arr || !arr.length) return;
-
-	const filteredArr = arr.filter((item) => item % 2 === 0);
-
-	return filteredArr;
-}
-
-console.log(filtraPares([1, 2, 3, 4]));
